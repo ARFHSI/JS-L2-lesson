0aec74532fa1ed226a17ac367b58370e88d87ede
ARFHSI <Abdulraouf.hsi@gmail.com>
2024-05-22 17:30:02 +0300

    LAB-1

---
+++ main.js
@@ -0,0 +1,36 @@
+
+// let sum = 0
+// // let increment = 2
+// for (let index = 0; index <= 25; index += 2) {
+
+//     sum += index
+//     console.log(`index ${index}`,sum);
+// }
+
+
+
+
+// =======================even numbers================
+
+for (let index = 0; index <= 25; index += 2) {
+
+    console.log(`index ${index}`);
+}
+
+// =======================odd numbers================
+
+for (let index = 1; index <= 10; index += 2) {
+
+    console.log(`index ${index}`);
+}
+
+// =======================squre numbers================
+
+for (let i = 1; i <= 10; i++) {
+    let square = i * i;
+    console.log(i, "squared is", square);
+  }
+
+
+
+  
