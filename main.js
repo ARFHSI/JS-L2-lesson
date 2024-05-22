689f7d054b15841c72b99f41f4b666fb324ea2a9
ARFHSI <Abdulraouf.hsi@gmail.com>
2024-05-22 20:27:31 +0300

    lab 3

--- main.js
+++ main.js
@@ -18,44 +18,54 @@ for (let i = 1; i <= 10; i++) {
     let square = i * i;
     console.log(i, "squared is", square);
   }
-  // =======================================================
 
-  // عملية الجمع
+   // عملية الجمع
 function sum(num1 , num2){
-return num1 + num2
-}
-
-var x = sum(2,2)
-var y = sum(4,4)
-console.log(x,y);
-
-  // عملية الطرح
-
-function min(num1 , num2){
-    return num1 - num2
+    return num1 + num2
     }
     
-    var a = min(2,1)
-    var b = min(3,8)
-    console.log(a,b);
-
-
-      // عملية الضرب
-
-      function mult(num1 , num2){
-        return num1 * num2
+    var x = sum(2,2)
+    var y = sum(4,4)
+    console.log(x,y);
+    
+      // عملية الطرح
+    
+    function min(num1 , num2){
+        return num1 - num2
         }
         
-        var c = mult(2,1)
-        var d = mult(3,8)
-        console.log(c,d);
-
-        // عملية القسمة
-
-        function divid(num1 , num2){
-            return num1 / num2
+        var a = min(2,1)
+        var b = min(3,8)
+        console.log(a,b);
+    
+    
+          // عملية الضرب
+    
+          function mult(num1 , num2){
+            return num1 * num2
             }
             
-            var e = divid(2,1)
-            var f = divid(3,8)
-            console.log(e,f);
+            var c = mult(2,1)
+            var d = mult(3,8)
+            console.log(c,d);
+    
+            // عملية القسمة
+    
+            function divid(num1 , num2){
+                return num1 / num2
+                }
+                
+                var e = divid(2,1)
+                var f = divid(3,8)
+                console.log(e,f);
+
+// ====++++====/////////////////////////---------------------------
+
+
+var numbers=[0,1,2,3,4,5,6,7,8,9]
+var mappar=[]
+
+for (let i = 0; i < numbers.length; i++) {
+    mappar.push(numbers[i]*10);
+}
+console.log(mappar);
